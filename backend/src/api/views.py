43b3bea710b39ee78fe9228af7327b44bdc3a338
FrostEasy54 <frosteasy@gmail.com>
2024-06-21 20:42:49 +0300

    add MongoDB functionality

---
+++ backend/src/api/views.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter
+
+from ..db.client import MongoDBClient
+from .models import Game, StartGame
+
+router = APIRouter(prefix="/games", tags=["Games"])
+
+
+@router.post("/")
+async def start_new_game(player_data: StartGame) -> Game:
+    data = {"player1": player_data.player, "player2": player_data.player}
+
+    client = MongoDBClient()
+    inserted_result = await client.insert(Game, data)  # type: ignore[arg-type]
+    game_data = await client.get(Game, inserted_result.inserted_id)  # type: ignore[arg-type]
+    result = Game(**game_data)
+
+    return result
---
+++ backend/src/main.py
@@ -0,0 +1,28 @@
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator
+
+from fastapi import FastAPI
+
+from .api.views import router as api_router
+from .db.utils import get_mongodb_client
+from .pet_project.settings import settings
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
+    # set up mongo DB
+    client = get_mongodb_client()
+    db = client.get_database(settings.MONGO_DB_DB)
+    app.mongodb = db  # type: ignore [attr-defined]
+    yield
+    # close MongoDB connection
+    client.close()  # type: ignore [attr-defined, operator]
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(api_router)
+
+
+@app.get("/")
+async def read_root() -> dict[str, str]:
+    return {"hello": "sevtony world"}
